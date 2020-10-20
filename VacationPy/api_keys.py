9ae4399a1ca1a3ee542fd81f743d0a3f1cc564e3
Mia Israel <70295357+miaisrael@users.noreply.github.com>
2020-10-20 18:34:46 -0400

    Update config

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "af456b3e7325dc3c8107e8931297d99a"
+weather_api_key = "api key here"
 
 # Google API Key
-g_key = "AIzaSyDkNPGd_AdjjIqamcLUBoW9RTIHP5rEmpo"
+g_key = "api key here"
