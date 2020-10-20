e4c589e503795d88416304feb933374fc0211e3e
Mia Israel <70295357+miaisrael@users.noreply.github.com>
2020-10-20 01:47:40 -0400

    Update config

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "daa3dd81f89a7aa37e41525572b409c9"
+weather_api_key = "18c164916a78e3c9eca41496e784bbbd"
 
 # Google API Key
 g_key = "AIzaSyDkNPGd_AdjjIqamcLUBoW9RTIHP5rEmpo"
